89cc58dd5d1dfd9e752b43b28ddfbe45c664b596
botkomakers <siam420riiya@gmail.com>
2025-05-09 21:54:49 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,13 @@
-#ণতযথ
+from flask import Flask
+import threading
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is alive!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+threading.Thread(target=run).start()
